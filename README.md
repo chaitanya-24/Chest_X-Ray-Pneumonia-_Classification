c36cd3e0201daa25ced703a063cf3a9f40427c9a
Chaitanya <csawant.cs7@gmail.com>
2024-05-07 18:46:09 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Chest_X-Ray-Pneumonia-_Classification
+
+
+- Update config.yaml
+- Update params.yaml
+- Update the entity
+- Update the configuration manager in src config
+- Update the components
+- Update the pipeline
+- Update the main.py
+- Update the dvc.yaml
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from src.cnnClassifier import logger 
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to CNN Classifier")
+
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> Stage {STAGE_NAME} completed successfully! <<<<")
+
+
+except Exception as e:
+    logger.exception(e)
+    raise e
